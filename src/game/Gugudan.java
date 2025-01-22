2763d2a16c697d4ca8064863a7906e4aec7e0dba
cath990409@gmail.com <cath990409@gmail.com>
2025-01-22 18:07:28 +0900

    Merge branch 'main' of https://github.com/Chan242/TuttleFire.git into main

--- src/game/Gugudan.java
+++ src/game/Gugudan.java
@@ -1,5 +1,17 @@
 package game;
 
 public class Gugudan {
+	Gugudan() {
 
+	}
+
+	public void gugudan() {
+		int result = 0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				result = i * j;
+				System.out.println(i + "*" + j + "=" + result);
+			}
+		}
+	}
 }
