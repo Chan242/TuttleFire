66194ea9c0ea87518fff9b4123ea0f085422642f
cath990409@gmail.com <cath990409@gmail.com>
2025-01-22 18:20:37 +0900

    구구단 로직 0 수정

--- src/game/Gugudan.java
+++ src/game/Gugudan.java
@@ -9,9 +9,10 @@ public class Gugudan {
 		int result = 0;
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				result = i * j;
-				System.out.println(i + "*" + j + "=" + result); 
+				result = (i+1) * (j+1);
+				System.out.print((i+1) + "*" + (j+1) + "=" + result+"\t"); 
 			}
+			System.out.println();
 		}
 	}
 }
