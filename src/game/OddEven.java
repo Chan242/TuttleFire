664ab257f1c0a21eb60c66ef4a437474048954c5
htfh5362 <htfh5362@naver.com>
2025-01-22 18:14:18 +0900

    푸시

--- src/game/OddEven.java
+++ src/game/OddEven.java
@@ -1,5 +1,35 @@
 package game;
 
-public class OddEven {
+import java.util.Random;
+import java.util.Scanner;
 
+/*
+ * 홀짝 게임
+ * 1. 1~100 사이의 랜덤 숫자를 저장한다.
+ * 2. 저장된 랜덤 숫자를 보여주고,
+ * 3. 해당 숫자가 홀수인지 짝수인지 맞추는 게임이다.
+ */
+
+
+public class OddEven {
+	
+	public void oe() {
+		Scanner scan = new Scanner(System.in);
+		Random ran = new Random();
+
+		int num = ran.nextInt(100) * 1
+		System.out.println("0. 홀짝게임 시작");
+		System.out.println("문제: [" + ram + "]");
+		System.out.println("1. 홀");
+		System.out.println("2. 짝");
+		
+		System.out.println("번호를 고르세요.");
+		
+		if()
+
+	}
+	
+	
+	
+	
 }
