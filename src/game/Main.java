981e01329e4c1e05a2305e6ec6d7353542b8d02c
ogremage01 <curselife01@gmail.com>
2025-01-22 18:12:42 +0900

    푸시 확인

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,28 @@
 package game;
 
+import java.util.Scanner;
+
 public class Main {
+	public static void main(String[] args) {
+		Dice dice = new Dice();
+		Gugudan gugudan = new Gugudan();
+		OddEven oe = new OddEven();
+		int input = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("1.구구단/2.홀짝/3.주사위 숫자. 원하시는 기능을 선택하세요");
+		input = sc.nextInt();
+		if (input == 1) {
+			gugudan.gugudan();
+		}else if (input == 2) {
+			dice.dice();
+		}else if (input == 3) {
+			oe.oe();
+		}else if (input >= 4||input == 0) {
+			System.out.println("잘못된 입력입니다. 프로그램을 종료합니다....");
+		}
+		
+	}
 
 }
